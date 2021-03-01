1bd56c174fc48cfbdb1983fbd31db335d8299edd
Francesco Taddia <fctaddia@gmail.com>
2021-03-01 15:53:26 +0100

    Update DbKeys.kt

--- katabase/app/src/main/java/it/kenble/katabase/db/DbKeys.kt
+++ katabase/app/src/main/java/it/kenble/katabase/db/DbKeys.kt
@@ -1,9 +1,18 @@
 package it.kenble.katabase.db
 
+/**
+ * @author Francesco Taddia
+ * @see 'https://github.com/fctaddia'
+ */
 class DbKeys {
+
+    /**
+     * DTO Key for Sql
+     */
     object itemsKeys {
         const val key1 = "Key1"
         const val key2 = "Key2"
         const val key3 = "Key3"
     }
+    
 }
