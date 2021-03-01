a0e7aee006c6f4b0b05f9d2618e4aa056e359d27
Francesco Taddia <fctaddia@gmail.com>
2021-03-01 13:24:28 +0100

    Update DbHandler.kt

--- katabase/app/src/main/java/it/kenble/katabase/db/DbHandler.kt
+++ katabase/app/src/main/java/it/kenble/katabase/db/DbHandler.kt
@@ -1,8 +1,8 @@
 package it.kenble.katabase.db
 
-import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
+import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
@@ -12,33 +12,36 @@ class DbHandler(context: Context, factory: SQLiteDatabase.CursorFactory?) : SQLi
         val CREATE_EXAMPLE_TABLE = "CREATE TABLE " + DbColumns.DbItem.TABLE_NAME + "(" + DbColumns.DbItem.ID + " TEXT PRIMARY KEY)"
         db.execSQL(CREATE_EXAMPLE_TABLE)
     }
+    
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         db.execSQL("DROP TABLE IF EXISTS ${DbColumns.DbItem.TABLE_NAME}")
         onCreate(db)
     }
 
     /*
-     * To use the getAllItems function, use the code commented below
-     *
-        var cursor = getAllItems()
-        var i = 0
-        var arrayItems : Array<String> = array("")
-        while(i<cursor.count){
-            arrayItems[i] = cursor.toString(cursor.getColumnIndex(DbColumns.DbRoom.ID))
-            cursor.moveToNext()
-            i++
-        }
+    * This function returns a cursor containing all the elements of the database
+    * To use the getAllItems function, use the code commented below
+    *
+    *   var cursor = getAllItems()
+    *   var i = 0
+    *   var arrayItems : Array<String> = array("")
+    *   while(i<cursor.count){
+    *       arrayItems[i] = cursor.toString(cursor.getColumnIndex(DbColumns.DbRoom.ID))
+    *       cursor.moveToNext()
+    *       i++
+    *   }
     */
-    fun getAllItems(): Cursor? {
+    fun getAllItems() : Cursor? {
         val db = this.readableDatabase
         return db.rawQuery("SELECT * FROM ${DbColumns.DbItem.TABLE_NAME}", null)
     }
 
 
     /*
+    * This function adds an item in the database
     * In the id parameter enter the identification key of the article contained in DbKeys
     */
-    fun addItem(id:String){
+    fun addItem(id: String) {
         val value = ContentValues()
         value.put(DbColumns.DbItem.ID , id)
         val db = this.writableDatabase
@@ -50,20 +53,41 @@ class DbHandler(context: Context, factory: SQLiteDatabase.CursorFactory?) : SQLi
     * This function returns true when the article is present in the database
     * In the id parameter enter the identification key of the article contained in DbKeys
     */
-    fun is_Item_inDB(id : String): Boolean {
+    fun isItem(id: String): Boolean {
         val db = this.readableDatabase
         val cursor = db.rawQuery("SELECT * FROM ${DbColumns.DbItem.TABLE_NAME}  WHERE ${DbColumns.DbItem.ID} = ? ", arrayOf(id) )
-        return cursor.move(1)
+        val result = cursor.move(1)
+        cursor.close()
+        db.close()
+        return result
+    }
+    
+    /*
+    * This function is used to update individual elements in the database
+    * In the id parameter enter the identification key of the article contained in DbKeys
+    * In the column parameter insert the reference column of the element you want to update
+    * In the item parameter enter the new volume that you want to update
+    */
+    fun updateItem(id: String, id: String, column: String, item: String) {
+        if (isItem(id)) {
+            val value = ContentValues()
+            value.put(column, item)
+            val db = this.writableDatabase
+            db.update(DbColumns.DbItem.TABLE_NAME, value, DbColumns.DbItem.ID + "=?", arrayOf(id))
+            db.close()
+        }
     }
 
 
     /*
+    * This function is used to remove an item in the database
     * In the id parameter enter the identification key of the article contained in DbKeys
     */
-    fun removeItem(id : String){
-        if(is_Item_inDB(id)){
+    fun removeItem(id: String) {
+        if(isItem(id)){
             val db = this.writableDatabase
             db.delete(DbColumns.DbItem.TABLE_NAME, DbColumns.DbItem.ID+ "=?", arrayOf(id))
+            db.close()
         }
     }
 
