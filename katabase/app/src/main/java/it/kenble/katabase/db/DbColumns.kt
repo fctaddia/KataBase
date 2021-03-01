814fce53903400a5d371f62af0c4ae7017bea502
Francesco Taddia <fctaddia@gmail.com>
2021-03-01 15:52:34 +0100

    Update DbColumns.kt

--- katabase/app/src/main/java/it/kenble/katabase/db/DbColumns.kt
+++ katabase/app/src/main/java/it/kenble/katabase/db/DbColumns.kt
@@ -2,9 +2,18 @@ package it.kenble.katabase.db
 
 import android.provider.BaseColumns
 
+/**
+ * @author Francesco Taddia
+ * @see 'https://github.com/fctaddia'
+ */
 object DbColumns {
+
+    /**
+     * DTO Table for Sql
+     */
     object DbItem : BaseColumns {
         const val TABLE_NAME = "rooms"
         const val ID = "id"
     }
+    
 }
